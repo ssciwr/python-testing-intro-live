06c93fabebf1a3d3575b9c301aa28631725b5cec
Liam Keegan <liam@keegan.ch>
2023-10-10 11:29:05 +0200

    gitautosync update 71

--- test_text.py
+++ test_text.py
@@ -14,5 +14,6 @@ def temp_filename(tmp_path):
 def test_count_lines(temp_filename):
     assert count_lines(temp_filename) == 1
 
+
 def test_count_characters(temp_filename):
-    assert count_lines(temp_filename) == 1
+    assert count_characters(temp_filename) == 5
