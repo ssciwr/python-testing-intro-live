58b90f38acff87e741e027ec0077ff3719165945
Liam Keegan <liam@keegan.ch>
2023-10-10 10:17:09 +0200

    gitautosync update 12

--- test_area.py
+++ test_area.py
@@ -1,8 +1,9 @@
 from area import area_of_square
+import pytest
 
 
 def test_area_of_square():
     assert area_of_square(1) == 1
     assert area_of_square(2) == 4
     assert area_of_square(0.5) == 0.25
-    assert 0.0399 < area_of_square(0.2) <= 0.04001
+    assert area_of_square(0.2) == 0.04001
