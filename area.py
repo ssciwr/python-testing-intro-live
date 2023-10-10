af1b227b89e7156900aa580a7a803599134190eb
Liam Keegan <liam@keegan.ch>
2023-10-10 10:51:37 +0200

    gitautosync update 51

--- area.py
+++ area.py
@@ -1,7 +1,7 @@
 def area_of_square(length):
     if length < 0:
         raise ValueError("Length must be non-negative")
-    return int(length)*length
+    return length*length
 
 
 def area_of_rectangle(length, width):
