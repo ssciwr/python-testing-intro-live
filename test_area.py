8b97cbc3401e6172d7e8aa691d4e755116a6a26a
Liam Keegan <liam@keegan.ch>
2023-10-10 10:48:09 +0200

    gitautosync update 47

--- test_area.py
+++ test_area.py
@@ -1,12 +1,9 @@
 from area import area_of_square, area_of_rectangle
 import pytest
 
-
-def test_area_of_square_valid():
-    assert area_of_square(1) == 1
-    assert area_of_square(2) == 4
-    assert area_of_square(0.5) == 0.25
-    assert area_of_square(0.2) == pytest.approx(0.04)
+@pytest.mark.parametrize(["input", "output"], [(1, 1), (2, 4), (0.5, 0.25), (0.2,0.04)])
+def test_area_of_square_valid(input, output):
+    assert area_of_square(input) == pytest.approx(output)
 
 
 @pytest.mark.parametrize("length", [-2, -5, -0.8325])
@@ -18,6 +15,7 @@ def test_area_of_square_invalid_value(length):
 def test_area_of_square_invalid_type():
     with pytest.raises(TypeError):
         area_of_square("2")
+
 
 @pytest.mark.parametrize("width", [1, 4, 0.4, 2.7])
 @pytest.mark.parametrize("length", [1.2, 456, 0.4768, 2.27])
