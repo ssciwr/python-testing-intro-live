521c88961cc1daa2484f75d5197673389ea1ade4
Liam Keegan <liam@keegan.ch>
2023-10-10 11:45:03 +0200

    gitautosync update 84

--- test_text.py
+++ test_text.py
@@ -31,4 +31,5 @@ def test_count_lines_long(long_temp_filename):
 
 
 def test_count_bytes():
+    url = "https://github.com/ssciwr/python-testing-intro-live/raw/main/README.md"
     assert count_bytes(url) == 123
