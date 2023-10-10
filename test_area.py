b89101740e7a0604b6a7d2782b302d1e7f80ded2
Liam Keegan <liam@keegan.ch>
2023-10-10 10:28:02 +0200

    gitautosync update 26

--- test_area.py
+++ test_area.py
@@ -11,5 +11,5 @@ def test_area_of_square_valid():
 
 def test_area_of_square_invalid():
     with pytest.raises(ValueError):
-        area_of_square(-2)
+        area_of_square(2)
         area_of_square(-5)
