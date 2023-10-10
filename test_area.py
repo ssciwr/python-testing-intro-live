a1238642013f43e351ddb58d387b3b767fcb1765
Liam Keegan <liam@keegan.ch>
2023-10-10 10:18:23 +0200

    gitautosync update 14

--- test_area.py
+++ test_area.py
@@ -7,3 +7,5 @@ def test_area_of_square():
     assert area_of_square(2) == 4
     assert area_of_square(0.5) == 0.25
     assert area_of_square(0.2) == pytest.approx(0.04)
+
+    assert area_of_square(-2) == 4
