5d0b987ff4cdab698b7c8fe18ba8b210b933d9ee
Liam Keegan <liam@keegan.ch>
2023-10-10 11:27:19 +0200

    gitautosync update 68

--- text.py
+++ text.py
@@ -2,3 +2,9 @@ def count_lines(filename):
     with open(filename) as f:
         lines = f.readlines()
     return len(lines)
+
+
+def count_characters(filename):
+    with open(filename) as f:
+        lines = f.readlines()
+    return sum(len(line) for line in lines)
