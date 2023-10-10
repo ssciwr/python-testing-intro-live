1058cc857c5b1a54eefa81235dcae8df21a61dd3
Liam Keegan <liam@keegan.ch>
2023-10-10 11:02:46 +0200

    gitautosync update 63

--- test_text.py
+++ test_text.py
@@ -3,6 +3,7 @@ from text import count_lines
 
 def test_count_lines(tmp_path):
     filename = tmp_path / "tmp.txt"
+    print(filename)
     with open(filename, "w") as f:
         f.write("hello")
     assert count_lines(filename) == 1
