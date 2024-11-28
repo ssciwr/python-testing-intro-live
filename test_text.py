d72e8288d73190cd69a3fa1a8f4a90ab6b9d577d
Liam Keegan <liam@keegan.ch>
2024-11-28 11:17:33 +0100

    gitautosync update 76

--- test_text.py
+++ test_text.py
@@ -6,6 +6,7 @@ def temp_file(tmp_path):
     temp_file = tmp_path / "file.txt"
     with open(temp_file, "w") as f:
         f.write("Hello")
+    return temp_file
 
 def test_count_lines(tmp_path):
     assert count_lines(temp_file) == 2
