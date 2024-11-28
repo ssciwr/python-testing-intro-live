a04c85e06d9c47cdee45f5257285dc57aa84b4af
Liam Keegan <liam@keegan.ch>
2024-11-28 11:44:25 +0100

    gitautosync update 109

--- test_text.py
+++ test_text.py
@@ -13,7 +13,7 @@ def temp_file(tmp_path):
     return func
 
 def test_count_lines(temp_file):
-    assert count_lines(temp_file) == 2
+    assert count_lines(temp_file(2)) == 2
 
 def test_count_chars(temp_file):
     assert count_chars(temp_file) == 11
