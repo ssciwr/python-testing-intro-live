201ef83222001660445071a2fdf868059c88032e
Liam Keegan <liam@keegan.ch>
2024-11-28 11:22:39 +0100

    gitautosync update 86

--- test_text.py
+++ test_text.py
@@ -1,7 +1,7 @@
 from text import count_lines, count_chars
 import pytest
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="session")
 def temp_file(tmp_path):
     temp_file = tmp_path / "file.txt"
     with open(temp_file, "w") as f:
