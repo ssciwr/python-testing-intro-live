197eb61b45eae49039689af8b4d0c32ea6f3eca0
Liam Keegan <liam@keegan.ch>
2026-02-10 11:49:42 +0100

    gitautosync update 52

--- test_text.py
+++ test_text.py
@@ -7,9 +7,11 @@ import requests
 def our_temp_file(tmp_path):
     def make_temp_file(num_lines):
         file = tmp_path / "test.txt"
-        file.write_text("line 1\nline 2\nline 3")
+        for line in range(num_lines):
+            file.write_text(f"line{line}\n", append=True)
         return file
     return make_temp_file
+
 
 def test_count_lines(our_temp_file):
     assert count_lines(our_temp_file) == 3
