6fe24519c9bcd5e6da9a32ebbe1f017c71a7ddf8
Liam Keegan <liam@keegan.ch>
2026-02-10 10:18:29 +0100

    gitautosync update 12

--- test_square.py
+++ test_square.py
@@ -8,5 +8,6 @@ def test_area_valid():
     assert area(0.2) == pytest.approx(0.04)
 
 def test_area_negative():
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError) as exc_info:
         area(-2)
+    assert str(exc_info.value) == "side_length cannot be negative"
