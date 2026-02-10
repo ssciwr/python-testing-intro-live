fc372d2d4324e34a898604152d5ae6ad23e68d26
Liam Keegan <liam@keegan.ch>
2026-02-10 11:48:50 +0100

    gitautosync update 51

--- test_text.py
+++ test_text.py
@@ -5,10 +5,11 @@ import requests
 
 @pytest.fixture()
 def our_temp_file(tmp_path):
-    file = tmp_path / "test.txt"
-    file.write_text("line 1\nline 2\nline 3")
-    return file
-
+    def make_temp_file(num_lines):
+        file = tmp_path / "test.txt"
+        file.write_text("line 1\nline 2\nline 3")
+        return file
+    return make_temp_file
 
 def test_count_lines(our_temp_file):
     assert count_lines(our_temp_file) == 3
