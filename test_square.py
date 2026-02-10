0a372fea3b1024e4d92a03cf65641f38d921e37a
Liam Keegan <liam@keegan.ch>
2026-02-10 10:24:23 +0100

    gitautosync update 18

--- test_square.py
+++ test_square.py
@@ -10,5 +10,7 @@ def test_area_valid():
 def test_area_negative():
     with pytest.raises(ValueError) as exc_info:
         area(-2)
+    assert "negative" in str(exc_info.value).lower()
+    with pytest.raises(ValueError) as exc_info:
         area(1)
     assert "negative" in str(exc_info.value).lower()
