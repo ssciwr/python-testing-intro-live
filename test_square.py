8411df6e8c74d9253908f48aefa02a3b5d97c733
Liam Keegan <liam@keegan.ch>
2026-02-10 10:09:01 +0100

    gitautosync update 5

--- test_square.py
+++ test_square.py
@@ -2,7 +2,7 @@ from square import area
 import pytest
 
 
-def test_area():
+def test_area_valid():
     assert area(2) == 4
     assert area(0.5) == pytest.approx(0.25)
     assert area(0.2) == pytest.approx(0.04)
