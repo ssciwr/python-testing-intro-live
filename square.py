df2178cda77e9146cb55fdf8dd51ab7d9f020e6d
Liam Keegan <liam@keegan.ch>
2026-02-10 10:36:08 +0100

    gitautosync update 24

--- square.py
+++ square.py
@@ -2,3 +2,9 @@ def area(side_length):
     if side_length < 0:
         raise ValueError("please note side_length cannot be negative")
     return side_length ** 2
+
+
+def area_rectangle(length, width):
+    if length < 0 or width < 0:
+        raise ValueError("please note length and width cannot be negative")
+    return length * width
